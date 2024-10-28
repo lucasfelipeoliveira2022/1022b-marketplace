c757c8dea52f19926cd570da4d17d59d41a1d2d5
lucasfelipeoliveira2022 <118355862+lucasfelipeoliveira2022@users.noreply.github.com>
2024-10-28 08:26:15 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -67,3 +67,24 @@ app.get("/usuarios",async(req,res)=>{
 app.listen(8000,()=>{
     console.log("SERVIDOR INICIADO NA PORTA 8000")
 })
+
+app.post("/produtos",async(req,res)=>{
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022b",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const {id,nome,descricao,preco,imagem} = req.body
+        const [result,fields]  = await conexao.query("INSERT INTO produtos VALUES (?,?,?,?,?)", [id,nome,descricao,preco,imagem])
+        await conexao.end() 
+        res.send(result)
+    }catch(e){
+        console.log(e)
+        res.status(500).send("Erro do servidor")
+    }
+    
+    
+})
